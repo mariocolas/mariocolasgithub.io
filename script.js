c0d49b51a71823e2c0ce382cae12b93b2c146f84
mariocolas <73491759+mariocolas@users.noreply.github.com>
2020-12-17 15:09:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+    var words = [
+        ' artist',
+        ' designer',
+        ' illustrator',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
